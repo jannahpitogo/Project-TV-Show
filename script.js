969256572769a50865a31b465640fe559193431c
jannahpitogo <pitogojannah@outlook.com>
2026-02-06 01:16:06 +0100

    removed the setup function and just implemented a loop to display all the object in the episodes. Also added some styles to look okay.

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
+const allEpisodes = getAllEpisodes();
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+//Making the card of films.
+for (const item of allEpisodes) {
+  const {
+    name,
+    season,
+    number,
+    image: { medium },
+    summary,
+  } = item;
+  const card = document.querySelector("template").content.cloneNode(true);
+  const seasonNumber =
+    `S${season.toString().padStart(2, "0")}` +
+    `E${number.toString().padStart(2, "0")}`;
+  //Line 8 just make an output like "S05E06", I just combined 2 template literals to make and combined the season and episode.
+  card.querySelector("h2").textContent = `${name} - ${seasonNumber}`;
+  card.querySelector("img").src = medium;
+  card.querySelector("p").innerHTML = summary;
+  //I used innerhtml since in the allEpisodes the <p> is included in the paragraph. (In the episodes.js)
 
-window.onload = setup;
+  document.getElementById("root").append(card);
+}
